980c41af28deecd6990009f2294c227621ca906c
susu032191 <373841272@qq.com>
2024-07-08 09:06:10 +0800

    更新天气

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -119,8 +119,6 @@ const getWeatherData = async () => {
       };
       // 获取天气信息
       const result = await getWeather(mainKey, weatherData.adCode.adcode);
-      console.log(result);
-      
       weatherData.weather = {
         weather: result.lives[0].weather,
         temperature: result.lives[0].temperature,
